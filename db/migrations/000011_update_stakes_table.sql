9abd52cc18a440cd0837b364d3e1fef006263cc8
Shane Vitarana <shanev@gmail.com>
2021-01-01 18:02:08 -0500

    added constraint name

--- db/migrations/000011_update_stakes_table.sql
+++ db/migrations/000011_update_stakes_table.sql
@@ -1,9 +1,9 @@
 -- +migrate Up 
 ALTER TABLE stakes
-  DROP CONSTRAINT stakes_vendor_id_post_id_key,
-  ADD UNIQUE (vendor_id, post_id, delegator, validator);
+  DROP CONSTRAINT stake_uq,
+  ADD CONSTRAINT stake_uq UNIQUE (vendor_id, post_id, delegator, validator);
 
 -- +migrate Down
 ALTER TABLE stakes
-  DROP CONSTRAINT stakes_vendor_id_post_id_delegator_validator_key,
-  ADD UNIQUE (vendor_id, post_id);
+  DROP CONSTRAINT stake_uq,
+  ADD CONSTRAINT stake_uq UNIQUE (vendor_id, post_id);
