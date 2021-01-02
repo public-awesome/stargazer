157385d7fc125647ee36b234f312f91591cf1d94
Shane Vitarana <shanev@gmail.com>
2021-01-01 19:18:22 -0500

    removed unneeded migration

--- db/migrations/000011_update_stakes_table.sql
+++
@@ -1,9 +0,0 @@
--- +migrate Up 
-ALTER TABLE stakes
-  DROP CONSTRAINT stake_uq,
-  ADD CONSTRAINT stake_uq UNIQUE (vendor_id, post_id, delegator, validator);
-
--- +migrate Down
-ALTER TABLE stakes
-  DROP CONSTRAINT stake_uq,
-  ADD CONSTRAINT stake_uq UNIQUE (vendor_id, post_id);
