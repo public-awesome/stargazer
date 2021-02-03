301b367af8d9522f0aa9aa7626c748443cae2228
Shane Vitarana <shanev@gmail.com>
2021-02-02 22:04:21 -0500

    fix migration (#50)

--- db/migrations/000011_add_post_reward.sql
+++ db/migrations/000011_add_post_reward.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
 ALTER TABLE posts
   ADD COLUMN total_upvote_reward_amount bigint NOT NULL DEFAULT 0,
-  ADD COLUMN total_upvote_reward_denom text NOT NULL;
+  ADD COLUMN total_upvote_reward_denom text NOT NULL DEFAULT 'ustarx';
 
 -- +migrate Down
 ALTER TABLE posts
--- db/migrations/000013_fix_post_reward.sql
+++
@@ -1,9 +0,0 @@
--- +migrate Up
-ALTER TABLE posts
-  DROP COLUMN total_upvote_reward_denom,
-  ADD COLUMN total_upvote_reward_denom text NOT NULL DEFAULT 'ustarx';
-
--- +migrate Down
-ALTER TABLE posts
-  DROP COLUMN total_upvote_reward_denom,
-  ADD COLUMN total_upvote_reward_denom text NOT NULL;
